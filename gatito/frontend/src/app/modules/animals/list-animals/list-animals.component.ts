b1712ee9ae5c7a2ebf53557634895c2a048d54f3
Renan Loureiro <renanpl47@gmail.com>
2022-07-19 09:45:48 -0300

    24%

--- gatito/frontend/src/app/modules/animals/list-animals/list-animals.component.ts
+++ gatito/frontend/src/app/modules/animals/list-animals/list-animals.component.ts
@@ -1,15 +1,29 @@
+import { Observable, switchMap } from 'rxjs';
+import { AnimalsService } from './../animals.service';
+import { UserService } from './../../authenticate/user/user.service';
 import { Component, OnInit } from '@angular/core';
+import { Animals } from '../animals';
 
 @Component({
   selector: 'app-list-animals',
   templateUrl: './list-animals.component.html',
-  styleUrls: ['./list-animals.component.css']
+  styleUrls: ['./list-animals.component.css'],
 })
 export class ListAnimalsComponent implements OnInit {
+  animals$!: Observable<Animals>;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private animalsService: AnimalsService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.animals$ = this.userService.getUser().pipe(
+      switchMap((user) => {
+        const userName = user.name ?? '';
 
+        return this.animalsService.listByUser(userName);
+      })
+    );
+  }
 }
